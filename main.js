ebf9dfe01e3379d1bd1061724f0544dc5efe0bed
TUHINPRO <71925866+TUHINPRO@users.noreply.github.com>
2021-05-28 14:28:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
- 
+ var song="";
+ function preload() {
+     song=loadSound("music.mp3");
+ }
+
+ function setup() {
+     canvas=createCanvas(500,500);
+     canvas.center();
+
+     video=createCapture(VIDEO);
+     video.hide();
+ }
+
+ function draw() {
+     image(video,0,0,500,500);
+
+ }
+
+ function playsong() {
+     song.play();
+ }
